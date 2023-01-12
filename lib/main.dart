b660e059bc9d3540e738660e0086e616a0c81fb7
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2023-01-12 16:57:28 -0300

    Add checage_page e home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:app_memories/page/checagem_page.dart';
 import 'package:app_memories/page/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const Memories());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    const Memories(),
+  );
 }
 
 class Memories extends StatelessWidget {
@@ -16,7 +24,7 @@ class Memories extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const ChecagemPage(),
     );
   }
 }
---
+++ lib/page/checagem_page.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChecagemPage extends StatefulWidget {
+  const ChecagemPage({super.key});
+
+  @override
+  State<ChecagemPage> createState() => _ChecagemPageState();
+}
+
+class _ChecagemPageState extends State<ChecagemPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+        print('Você não esta logado!');
+      } else {
+        print('Você esta logado!');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
